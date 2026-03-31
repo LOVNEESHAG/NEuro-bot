9afb3c6d11d02475cf5b57d4c46ce0443242c372
LOVNEESHAG <lovneesh@example.com>
2026-03-31 23:14:53 +0530

    added api test button

--- ENHHclient/src/App.jsx
+++ ENHHclient/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { API } from "./config";
+import { API } from "../../client/config";
 
 function App() {
   const [count, setCount] = useState(0)
